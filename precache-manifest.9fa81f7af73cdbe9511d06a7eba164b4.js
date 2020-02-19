97a5522331f34acf9ef251b498b366854fa2b243
Nageshwari <nageshwari.p17@gmail.com>
2020-02-19 11:22:34 +0530

    Updates

---
+++ precache-manifest.9fa81f7af73cdbe9511d06a7eba164b4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1352a69abb2118259f8ef26c58a95156",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "4525891124caa7441104",
+    "url": "/shopping-cart/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "4e0ab5346bc3ec3a95ae",
+    "url": "/shopping-cart/static/css/main.3ebb4712.chunk.css"
+  },
+  {
+    "revision": "4525891124caa7441104",
+    "url": "/shopping-cart/static/js/2.40ff4728.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/shopping-cart/static/js/2.40ff4728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0ab5346bc3ec3a95ae",
+    "url": "/shopping-cart/static/js/main.b44d2ae6.chunk.js"
+  },
+  {
+    "revision": "d33a4d2204dad954a930",
+    "url": "/shopping-cart/static/js/runtime-main.db2c7b0f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/shopping-cart/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
